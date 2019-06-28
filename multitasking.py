1489781770cc1e1265dd79cdb93da606661b8d77
Terje Molnes <tmolnez@gmail.com>
2019-06-28 03:46:23 +0200

    Add findNumberOfValidLinks
    Find number of valid links and saves infro to
    global variables.

--- multitasking.py
+++ multitasking.py
@@ -1,5 +1,10 @@
 """Module for all activity implementing multitasking."""
 
+import requests
+import threading
+
+import globals
+
 
 class WorkTask:
     """Store tasks that will be assigned to logical processors."""
@@ -10,3 +15,48 @@ class WorkTask:
         self.startCount = None
         self.endCount = None
         self.workList = None
+
+
+def findNumberOfValidLinks(
+                        imageLinks,
+                        numberOfValidLinks_t,
+                        validLinks_t,
+                        unvalidLinks_t,
+                        index):
+    """Find number of valid links and save to global variables.
+
+    Arguments:
+        imageLinks {[string]} -- [description]
+        numberOfValidLinks_t {[type]} -- [description]
+        validLinks_t {[type]} -- [description]
+        unvalidLinks_t {[type]} -- [description]
+        index {[type]} -- [description]
+    """
+    numberOfLinks = len(imageLinks)
+    cnt = 0
+    globals.g_progress
+
+    _numberOfValidLinks = 0
+    _validLinks = []
+    _unvalidLinks = []
+    for link in imageLinks:
+        try:
+            r = requests.head(link, timeout=0.2)
+            if(r.ok):
+                _numberOfValidLinks += 1
+                _validLinks.append(link)
+            else:
+                unvalidLink = f'{link} [Status_code: {r.Status_code}]'
+                _unvalidLinks.append(unvalidLink)
+        except Exception as e:
+            print(e)
+        cnt += 1
+        trdName = threading.current_thread().name
+        trdNum = int(trdName[-3:])
+        if numberOfLinks == 0:
+            globals.g_progress[trdNum] = 1.0
+        else:
+            globals.g_progress[trdNum] = cnt/numberOfLinks
+    numberOfValidLinks_t[index] = _numberOfValidLinks
+    validLinks_t[index] = _validLinks
+    unvalidLinks_t[index] = _unvalidLinks
